b74f5c70fc4d061423332790b2cec6ccddbeb91c
Liu Pengpeng <liupppppp@gmail.com>
2019-05-12 13:03:02 +0800

    fix bsv CSP import

--- packages/bitcore-node/src/providers/chain-state/bsv/bsv.ts
+++ packages/bitcore-node/src/providers/chain-state/bsv/bsv.ts
@@ -1,5 +1,4 @@
 import { BCHStateProvider } from '../bch/bch';
-import { CSP } from '../../../types/namespaces/ChainStateProvider';
 
 export class BSVStateProvider extends BCHStateProvider {
   constructor(chain: string = 'BCH') {
